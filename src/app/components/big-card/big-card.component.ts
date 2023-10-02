85eaaca2cee8c0898997305e77644049288e5b63
Fábio Mateus Melo <fabioceto@gmail.com>
2023-10-02 16:49:05 -0300

    0006

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BigCardComponent implements OnInit {
 
   @Input()
-  photoCover:string =""
+  photoCover: string =""
   @Input()
   cardTitle:string= ""
   @Input()
